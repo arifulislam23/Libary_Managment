104eded283450b56bb348f30eb146241012203c7
Ariful Islam Akash <ariful.akash23@gmail.com>
2025-06-29 11:35:59 +0600

    1st push

---
+++ Libary_Managment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Libary_Managment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
